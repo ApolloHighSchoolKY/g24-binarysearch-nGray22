fbf862a9aa697d3bbfcf632003b7c8d7d6ecd14c
nGray22 <noah.gray@stu.daviess.kyschools.us>
2025-04-16 14:03:59 +0000

    Done (correctly this time)

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,7 +3,30 @@ import java.util.Arrays;
 public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
-	{
+	{	
+		Arrays.sort(ray);
+		int bot = 0;
+		int top = ray.length-1;
+		int middle = (bot + top) /2;
+		while(bot<=top)
+		{
+			if(ray[middle]<item)
+			{
+				bot = middle+1;
+			}
+			else if(ray[middle]>item)
+			{
+				top = middle -1;
+			}
+			else 
+			{
+			 return middle;
+			 	
+			}	
+			middle = (bot + top) /2;
+		}
 
+		return -1;
+		
 	}
 }
